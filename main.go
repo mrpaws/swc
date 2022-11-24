f53d98d264b8152eb97bc21308f3a74e5dfdd676
Matt Pawelski <mpawelsk@gmail.com>
2022-11-24 12:00:19 -0800

    add athlete activity handler

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-// strava activity webhook subscription callback endpoint
+/* strava activity webhook subscription callback endpoint manager
+   ref: https://developers.strava.com/docs/webhooks/
+*/
 
 import (
 	"encoding/json"
@@ -12,7 +14,7 @@ import (
 
 func main() {
 	log.Print("starting swc athlete event receiver server...")
-	http.HandleFunc("/swc/receiver", SubscriptionEventHandler)
+	http.HandleFunc("/swc/receiver", SubscriptionEventRouter)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -27,10 +29,24 @@ func main() {
 	}
 }
 
-func SubscriptionEventHandler(w http.ResponseWriter, r *http.Request) {
+func SubscriptionEventRouter(w http.ResponseWriter, r *http.Request) {
 	/*
 		handles events from strava webhook subscription
 		https://developers.strava.com/docs/webhooks/
+	*/
+	method := r.Method
+
+	// strava subscription service callback events
+	if method == "GET" {
+		SubscriptionEventHandler(w, r)
+	} else if method == "POST" {
+		AthleteActivityEventHandler(w, r)
+	}
+}
+
+func SubscriptionEventHandler(w http.ResponseWriter, r *http.Request) {
+	/* handler for receiving subscription event
+	i.e. creation and callback challenge / token verification
 	*/
 	query := r.URL.Query()
 
@@ -49,5 +65,28 @@ func SubscriptionEventHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Fprintf(w, "%s", b)
 	}
+}
+func AthleteActivityEventHandler(w http.ResponseWriter, r *http.Request) {
+	/* handler for receiving event about athlete or activity
 
+		   https://developers.strava.com/docs/webhooks/
+	       - 200 OK + async processing recommended;
+		   - multiple fires per update possible;
+		   - multiple values in updates kv
+
+		   POST payload structure i.e.:
+		     {
+				"aspect_type": "update",
+				"event_time": 1516126040,
+				"object_id": 1360128428,
+				"object_type": "activity",
+				"owner_id": 134815,
+				"subscription_id": 120475,
+				"updates": {
+					"title": "Messy"
+			}
+	*/
+	r.ParseForm()
+	log.Println("handling athlete or activity update:", r.PostForm)
+	fmt.Fprintf(w, "")
 }
